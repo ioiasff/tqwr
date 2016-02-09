823cb8cdb20a1d82cb4dcbdc5c695aa8cc6341c7
ioiasff <arash.github@gmail.com>
2016-02-09 12:35:19 +0330

    Create addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -7,7 +7,7 @@ end
         if not is_momod or not is_owner then
     return "Only owners can add admin!"
 end
-    local user = 'user#id'152485254
+    local user = 'sudo#id'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
